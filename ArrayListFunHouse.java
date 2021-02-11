6579723011b97e48960b117890f226c141d1412f
Kyle Kim <300022347@fjuhsd.org>
2021-02-11 03:51:38 +0000

    Completed unit 7 lab 3

--- ArrayListFunHouse.java
+++ ArrayListFunHouse.java
@@ -12,10 +12,40 @@ public class ArrayListFunHouse
 {
  public static ArrayList<Integer> getListOfFactors(int number)
  {
-  return null;
+  ArrayList<Integer> factorList = new ArrayList<Integer>();
+  for(int i=1; i<=number;i++)
+  {
+    if(number%i==0)
+    {
+      factorList.add(i);
+    }
+    else
+    {}
+  }
+  return factorList;
  }
  
  public static void keepOnlyCompositeNumbers( List<Integer> nums )
  {
+  for(int i=1; i<nums.size();i++)
+  {
+    int numOfFactors = 0;
+    int num = nums.get(i);
+    for(int n = 1; n<=num;i++)
+    {
+      if(num%n==0)
+      {
+        numOfFactors++;
+      }
+      else{}
+    }
+
+    if(numOfFactors==2)
+    {
+      nums.remove(i);
+    }
+    if(numOfFactors>=3){}
+    
  }
+}
 }
